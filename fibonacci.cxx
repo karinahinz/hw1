ac5fce7dc6e6889f3659f3de873567ef6bdf2fde
Karina Hinz <karina@linux-clon.site>
2015-11-03 12:15:48 +0100

    Fibonacci-Zahlen Hausaufgabe fertiiig

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,6 +1,14 @@
-#include <iostream>
 
-int main(){
-  std::cout << "hallo" << std::endl;
-  return 0;
-}
+#include <iostream>
+using namespace std;
+ int main(){
+  cout << "Fibonacci HW1" << endl;
+  int j=1, k=1, sum;
+  for(int i=0; i<10; i++){
+  cout << j << endl;
+    sum=j+k;
+    j=k;
+    k=sum;
+  }
+ return 0;
+ }
